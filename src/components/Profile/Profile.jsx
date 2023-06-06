efb54ceab7ab125790957558d048e982fe223057
Vladislav2097 <116560621+Vladislav2097@users.noreply.github.com>
2023-06-06 16:49:44 +0300

    New components

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import s from './Profile.module.css'
+import MyPosts from './MyPosts/MyPosts';
+
+const Profile = () => {
+  return (
+    <div className={s.content}>
+      <div>
+        <img src='https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg' />
+      </div>
+      <div>
+        ava + description
+      </div>
+      <MyPosts />
+    </div>
+  )
+}
+
+export default Profile;
