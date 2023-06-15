10f19630fe5f6d91929e4c6024369d2887b842af
Vladislav2097 <116560621+Vladislav2097@users.noreply.github.com>
2023-06-15 19:39:42 +0300

    add metod map

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,7 +3,7 @@ import s from './Dialogs.module.css';
 import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
 
 const DialogItem = (props) => {
-    let path = props.id;
+    let path = `${props.id}`;
     return (<div className={s.dialog + ' ' + s.active}>
         <NavLink to={path}>{props.name}</NavLink>
     </div>)
@@ -17,7 +17,7 @@ const Message = (props) => {
 
 const Dialogs = (props) => {
 
-    let dialogData = [
+    let dialogs = [
         { id: 1, name: 'Dimych' },
         { id: 2, name: 'Andrew' },
         { id: 3, name: 'Sveta' },
@@ -26,7 +26,7 @@ const Dialogs = (props) => {
         { id: 6, name: 'Valera' },
     ]
 
-    let messagesData = [
+    let messages = [
         { id: 1, message: 'Hi' },
         { id: 2, message: 'How is your it-kamasutra' },
         { id: 3, message: 'Yo' },
@@ -34,19 +34,19 @@ const Dialogs = (props) => {
         { id: 5, message: 'Yo' },
     ]
 
+    let dialogsElements = dialogs
+        .map( d => <DialogItem name={d.name} id={d.id} />)
+
+    let messagesElement = messages
+        .map( m => <Message message={m.message} />)
+
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <DialogItem name={dialogData[0].name} id={dialogData[0].id} />
-                <DialogItem name={dialogData[1].name} id={dialogData[1].id} />
-
+                { dialogsElements }
             </div>
             <div className={s.messages}>
-                <Message message={messagesData[0].message} />
-                <Message message={messagesData[1].message} />
-                <Message message='Yo' />
-                <Message message='Yo' />
-                <Message message='Yo' />
+                { messagesElement }
             </div>
         </div>
     )
