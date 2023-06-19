2da3397db7565db0ec286e0e6f420de844c32f02
Vladislav2097 <116560621+Vladislav2097@users.noreply.github.com>
2023-06-19 16:35:14 +0300

    transfer data to index.js

--- src/App.js
+++ src/App.js
@@ -4,31 +4,52 @@ import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import Dialogs from './components/Dialogs/Dialogs';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import News from './components/News/News';
 import Music from './components/Music/Music';
 import Settings from './components/Settings/Settings';
 
-const App = () => {
-  return (
-    <BrowserRouter>
-      <div className='app-wrapper'>
-        <Header />
-        <Navbar />
-        <div className='app-wrapper-content'>
-          <Routes>
-            <Route path='/dialogs/*' element= {<Dialogs/>} />
-            <Route path='/profile' element={<Profile/>} />
-            <Route path='/news' element={<News/>} />
-            <Route path='/music' element={<Music/>} />
-            <Route path='/settings' element={<Settings/>} />
-          </Routes>
-        </div>
-      </div>
-    </BrowserRouter>
-  );
-}
+let dialogs = [
+  { id: 1, name: 'Dimych' },
+  { id: 2, name: 'Andrew' },
+  { id: 3, name: 'Sveta' },
+  { id: 4, name: 'Sasha' },
+  { id: 5, name: 'Viktor' },
+  { id: 6, name: 'Valera' },
+]
 
+let messages = [
+  { id: 1, message: 'Hi' },
+  { id: 2, message: 'How is your it-kamasutra' },
+  { id: 3, message: 'Yo' },
+  { id: 4, message: 'Yo' },
+  { id: 5, message: 'Yo' },
+]
+const App = (props) => {
+
+
+    return (
+        <BrowserRouter>
+            <div className='app-wrapper'>
+                <Header/>
+                <Navbar/>
+                <div className='app-wrapper-content'>
+                    <Routes>
+                        {/*<Route path='/dialogs/*' element={<Dialogs/>}/>*/}
+                        {/*<Route path='/profile' element={<Profile/>}/>*/}
+
+                        <Route path='/dialogs/*'  element={<Dialogs dialogs={props.dialogs} messages={props.messages}/>} />
+                        <Route path='/profile' element={ <Profile posts={props.posts}/>} />
+
+                        <Route path='/news' element={<News/>}/>
+                        <Route path='/music' element={<Music/>}/>
+                        <Route path='/settings' element={<Settings/>}/>
+                    </Routes>
+                </div>
+            </div>
+        </BrowserRouter>
+    );
+}
 
 
 export default App;
